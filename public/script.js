fe10369ad617e239341e9614d522d49d63aa5e37
GTgyani206 <gyanendrathakur4135stella@gmail.com>
2024-02-10 18:40:58 +0530

    Updated files

--- public/script.js
+++ public/script.js
@@ -23,3 +23,9 @@ drkMd.addEventListener("click", () => {
   }
 });
 
+function scrollToElement(elementId) {
+  const element = document.getElementById(elementId);
+  if (element) {
+    element.scrollIntoView({ behavior: 'smooth' });
+  }
+}
