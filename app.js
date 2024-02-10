fb8fe52f4039efbc4afdb0a145a9237af2133d45
GTgyani206 <gyanendrathakur4135stella@gmail.com>
2024-02-10 22:20:13 +0530

    Update files

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-const express = require('express');
-const ejs =  require('ejs')
-const ejsMate = require('ejs-mate');
-const path = require('path');
+const express = require("express");
+const ejs = require("ejs");
+const ejsMate = require("ejs-mate");
+const path = require("path");
 const app = express();
 
-app.engine('ejs', ejsMate);
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
-    res.render('routes/home.ejs');
-    });
+app.get("/", (req, res) => {
+  res.render("routes/home.ejs");
+});
+
+app.get("/announcement", (req, res) => {
+  res.render("routes/announcement.ejs");
+});
 
 app.listen(3000, () => {
-    console.log('Serving on port 3000');
-    });
+  console.log("Serving on port 3000");
+});
